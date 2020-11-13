5a86be26d8bcb0621fe626f73df53645be917584
Arthur Korchkov <qaeng16@gmail.com>
2020-11-13 15:48:43 +0000

    This is a master code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  cout << "\n\nStart\n\n";
+
+  int n = 0;
+  cout << "Please, enter an even number: " << endl;
+  cin >> n;
+  while (n % 2 != 0){
+    cout << "The number is not even, please, try one more time. " << endl;
+    cin >> n;
+  }
+  for (int row = 0; row <= n; row++){
+    for (int col = 0; col <= n; col++){
+      if ((col == row)||(row + col == n)){
+        cout << "*";
+      }
+      else {
+        cout << " ";
+      }
+    }
+    cout << endl;
+
+  }
+
+  cout << "\n\nFinish\n\n";
+  return 0;
 }
