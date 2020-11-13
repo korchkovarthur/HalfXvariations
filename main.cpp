0b32729dfbbd46b480688088f376492c7ce94939
Arthur Korchkov <qaeng16@gmail.com>
2020-11-13 16:11:02 +0000

    rightHalf

--- main.cpp
+++ main.cpp
@@ -8,17 +8,20 @@ int main() {
   int n = 0;
   cout << "Please, enter an even number: " << endl;
   cin >> n;
-  while (n % 2 != 0){
+
+    while (n % 2 != 0){
     cout << "The number is not even, please, try one more time. " << endl;
     cin >> n;
   }
+  
+
   for (int row = 0; row <= n; row++){
-    for (int col = 0; col <= n; col++){
+    for (int col = 0; col <= n/2; col++){
       if ((col == row)||(row + col == n)){
         cout << "*";
       }
       else {
-        cout << " ";
+        cout << ".";
       }
     }
     cout << endl;
